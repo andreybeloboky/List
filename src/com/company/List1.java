afdad075fb971153b47598243686fc38f4fd0572
Andrey Beloboky <andrei.beloboky@yandex.ru>
2021-12-02 16:17:12 +0300

    add method in the List1 (add, delete, get, size, set)

--- src/com/company/List1.java
+++ src/com/company/List1.java
@@ -1,5 +1,9 @@
 package com.company;
 
 public interface List1 {
-    
+    void add (String e);
+    void delete(int index);
+    String get(int index);
+    int size();
+    void set(int index, String e);
 }
