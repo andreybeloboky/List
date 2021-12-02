d89bbe25b6c71d7ba36dac4bacb3ca2d7f234eb2
Andrey Beloboky <andrei.beloboky@yandex.ru>
2021-12-02 23:27:18 +0300

    add methods: add and delete in the ArrayList1

--- src/com/company/ArrayList1.java
+++ src/com/company/ArrayList1.java
@@ -11,11 +11,17 @@ public class ArrayList1 implements List1 {
         Arrays.fill(array, "1");
     }
 
+    /**
+     * @param size
+     */
     public ArrayList1(int size) {
         this.array = new String[size];
         Arrays.fill(array, "1");
     }
 
+    /**
+     * @param name
+     */
     @Override
     public void add(String name) {
         String[] temp = array;
@@ -26,22 +32,35 @@ public class ArrayList1 implements List1 {
 
     @Override
     public void delete(int index) {
-        array[index] = null;
-
+        String[] temp = array;
+        this.array = new String[temp.length - 1];
+        System.arraycopy(temp, 0, array, 0, index);
+        System.arraycopy(temp, index + 1, array, index, array.length - 1);
     }
 
+    /**
+     * @param index
+     * @return
+     */
     @Override
     public String get(int index) {
         return array[index];
     }
 
+    /**
+     * @return
+     */
     @Override
     public int size() {
         return array.length;
     }
 
+    /**
+     * @param index
+     * @param name -
+     */
     @Override
-    public void set(int index, String e) {
-        array[index] = e;
+    public void set(int index, String name) {
+        array[index] = name;
     }
 }
---
+++ src/com/company/LinkedList1.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class LinkedList1 implements List1 {
+    @Override
+    public void add(String e) {
+
+    }
+
+    @Override
+    public void delete(int index) {
+
+    }
+
+    @Override
+    public String get(int index) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void set(int index, String e) {
+
+    }
+}
